b347908d1513eeab73a8fed15c13c61a41c49647
= <deval.chawda@enate.net>
2025-07-22 21:58:56 +0530

    Clean commit

---
+++ WebhookReceiver/Helpers/Helpers.cs
@@ -0,0 +1,83 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using WebhookReceiver.Models;
+
+namespace WebhookReceiver.Helpers
+{
+    public class Helpers
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source= ");
+
+
+        public string loginToEnate()
+        {
+            string url = "https://enate_InstanceUrl";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            string apikey;
+
+
+            var body = new loginData
+            {
+                username = "", //Username of instance
+                password = "" //password of instance
+            };
+
+            request.AddJsonBody(body);
+
+
+            var response = client.Post(request);
+
+            apikey = response.Content;
+            apikey = apikey.Replace("+", "%2B").Replace("/", "%2F").Replace("=", "%3D");
+            apikey = apikey.Substring(1, apikey.Length - 2);
+            return apikey;
+        }
+
+        private void updateDatabase()
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE Requests SET PullState = 0 WHERE PullState = 1;", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        private object getWebhookGUID(string id, string apikey)
+        {
+            var url ="https://enate_InstanceUrl"; + apikey;
+            var client1 = new RestClient(url);
+            var request1 = new RestRequest();
+
+
+
+            // POST Request to Enate API
+
+            id = id.ToUpper();
+            var body1 = new MyWebhookSubsObj
+            {
+                Webhook = "NewCommunication",
+                FilterObjectGUID = id,
+                FilterObjectType = "Packet",
+                SubscriberURL = "https://webhook.site/fff2f57f-4b9e-4d54-ad99-0f3f11eb9636"
+            };
+
+            request1.AddJsonBody(body1);
+            var response = client1.Post(request1);
+            string content = response.Content.ToString();
+
+
+            // Desrialization of Response content and returning webhook GUID
+
+            JavaScriptSerializer deSerializedResponse = new JavaScriptSerializer();
+            Root root = (Root)deSerializedResponse.Deserialize(content, typeof(Root));
+            return root.Result.GUID;
+        }
+    }
+}
---
+++ WebhookReceiver/Models/MyObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebhookReceiver.Models
+{
+    public class MyObj
+    {
+      
+            public string CustomerName { get; set; }
+            public string SupplierName { get; set; }
+            public string ContractName { get; set; }
+            public string ServiceName { get; set; }
+            public string ServiceLineName { get; set; }
+            public string ProcessTypeName { get; set; }
+            public string Reference { get; set; }
+            public string Title { get; set; }
+            public int Status { get; set; }
+            public string DueDate { get; set; }
+            public string ResolvedOn { get; set; }
+            public int RAGStatus { get; set; }
+            public int ProcessType { get; set; }
+            public int StartedByMethod { get; set; }
+            public string GUID { get; set; }
+
+    }
+}
